6701843eb22dcc29b56a9116f757537ef759b442
Denis <denis09112004@gmail.com>
2024-05-23 18:02:23 +0300

    add models

---
+++ taskplanner-user-service/Models/Task.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace taskplanner_user_service.Models;
+
+[Table("tasks")]
+public class Task
+{
+    [Column("id")]
+    public int Id { get; set; }
+    [Column("title")]
+    public string Title { get; set; }
+    [Column("description")]
+    public string Description { get; set; }
+    [Column("status")]
+    public string Status { get; set; }
+    [Column("user_id")]
+    [ForeignKey("User")]
+    public int UserId { get; set; }
+    public User User { get; set; }
+    [Column("done_at")]
+    public DateTime DoneAt { get; set; }
+}
---
+++ taskplanner-user-service/Models/User.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace taskplanner_user_service.Models;
+
+[Table("users")]
+public class User
+{
+    [Column("id")]
+    public int Id { get; set; }
+    [Column("email")]
+    public string Email { get; set; }
+    [Column("password")]
+    public string Password { get; set; }
+}
