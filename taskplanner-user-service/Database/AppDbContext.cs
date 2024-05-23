943e8e833a530ea31290decafaf138ccab03be81
Denis <denis09112004@gmail.com>
2024-05-23 17:59:24 +0300

    database (postgres)

---
+++ taskplanner-user-service/Database/AppDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using taskplanner_user_service.Models;
+using Task = taskplanner_user_service.Models.Task;
+
+namespace taskplanner_user_service.Database;
+
+public class AppDbContext : DbContext
+{
+    private IConfiguration Configuration;
+
+    public AppDbContext(IConfiguration configuration)
+    {
+        Configuration = configuration;
+    }
+    
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseNpgsql(Configuration.GetConnectionString("Database"));
+    }
+
+    public DbSet<User> Users { get; set; }
+    public DbSet<Task> Tasks { get; set; }
+}
