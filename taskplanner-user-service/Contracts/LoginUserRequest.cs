0325a032b9d54e092e70b69e40f3acc32b60a5de
Denis <denis09112004@gmail.com>
2024-05-23 17:58:53 +0300

    add requests

---
+++ taskplanner-user-service/Contracts/LoginUserRequest.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace taskplanner_user_service.Contracts;
+
+public record LoginUserRequest(
+    [Required] string Email,
+    [Required] string Password,
+    [Required] string RepeatPassword
+    );
---
+++ taskplanner-user-service/Contracts/RegisterUserRequest.cs
@@ -0,0 +1,8 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace taskplanner_user_service.Contracts;
+
+public record RegisterUserRequest(
+    [Required] string Email,
+    [Required] string Password
+);
