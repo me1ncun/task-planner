e69f0ff10df99557ccda90ddae0d70a92095cf98
Denis <denis09112004@gmail.com>
2024-05-23 17:59:39 +0300

    added extensions

---
+++ taskplanner-user-service/Extensions/ApiExtension.cs
@@ -0,0 +1,36 @@
+﻿using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using taskplanner_user_service.Helpers;
+
+namespace taskplanner_user_service.Extensions;
+
+public static class ApiExtension
+{
+    public static void AddApiAuthentication(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(JwtBearerDefaults.AuthenticationScheme, options =>
+        {
+            options.TokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuer = false,
+                ValidateAudience = false,
+                ValidateLifetime = true,
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["JwtOptions:SecretKey"]))
+            };
+
+            options.Events = new JwtBearerEvents
+            {
+                OnMessageReceived = context =>
+                {
+                    context.Token = context.Request.Cookies["token"];
+                     
+                    return Task.CompletedTask;
+                }
+            };
+        });
+
+        services.AddAuthorization();
+    }
+}
---
+++ taskplanner-user-service/Extensions/MigrationExtension.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using taskplanner_user_service.Database;
+
+namespace taskplanner_user_service.Extensions;
+
+public static class MigrationExtension
+{
+    public static void ApplyMigrations(this IApplicationBuilder app)
+    {
+        using (var scope = app.ApplicationServices.CreateScope())
+        {
+            var services = scope.ServiceProvider;
+
+            var context = services.GetRequiredService<AppDbContext>();
+            if (context.Database.GetPendingMigrations().Any())
+            {
+                context.Database.Migrate();
+            }
+        }
+    }
+}
