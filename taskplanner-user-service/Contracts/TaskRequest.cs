a5376a3dd12f2a3b3b86f224f033a8ca6fcb2c4b
Denis <denis09112004@gmail.com>
2024-05-25 09:51:19 +0300

    created two requests

---
+++ taskplanner-user-service/Contracts/TaskRequest.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace taskplanner_user_service.Contracts;
+
+public class TaskRequest
+{
+    [Required]
+    public string Title { get; set; }
+    [Required]
+    public string Description { get; set; }
+    [Required]
+    public string Status { get; set; }
+    [Required]
+    public DateTime DoneAt { get; set; }
+}
---
+++ taskplanner-user-service/Contracts/TaskUpdateRequest.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace taskplanner_user_service.Contracts;
+
+public class TaskUpdateRequest
+{
+    [Required]
+    public int Id { get; set; }
+    [Required]
+    public string Title { get; set; }
+    [Required]
+    public string Description { get; set; }
+    [Required]
+    public string Status { get; set; }
+    [Required]
+    public DateTime DoneAt { get; set; }
+}
