f40f22f7ce14f1f4ae03f2c1c99f3d266bbd3af5
Denis <denis09112004@gmail.com>
2024-05-25 21:18:10 +0300

    add enter

--- taskplanner-user-service/Program.cs
+++ taskplanner-user-service/Program.cs
@@ -15,6 +15,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.Configure<JwtOptions>(builder.Configuration.GetSection(nameof(JwtOptions)));
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
