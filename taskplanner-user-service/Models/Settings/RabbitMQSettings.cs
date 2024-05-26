512bee585529096e3172fe1a68490fac2d8416ce
Denis <denis09112004@gmail.com>
2024-05-26 21:13:42 +0300

    created rabbit settings model

---
+++ taskplanner-user-service/Models/Settings/RabbitMQSettings.cs
@@ -0,0 +1,8 @@
+﻿public class RabbitMqSettings
+{
+    public string Host { get; set; }
+    public int Port { get; set; }
+    public string UserName { get; set; }
+    public string Password { get; set; }
+    public string QueueName { get; set; }
+}
