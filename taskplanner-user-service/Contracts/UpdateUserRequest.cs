ab6e2f51bac567f65c1d9bb72dd809daab71f4c8
Denis <denis09112004@gmail.com>
2024-05-29 21:47:22 +0300

    created model for update user password

---
+++ taskplanner-user-service/Contracts/UpdateUserRequest.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace taskplanner_user_service.Contracts;
+
+public record UpdateUserRequest(
+    [Required] string Email,
+    [Required] string NewPassword,
+    [Required] string ConfirmPassword
+);
